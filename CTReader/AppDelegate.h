e8fd33eb39e060ff7a58bd84ee13d2db59938e2b
wangchao <xxhank.boy@gmail.com>
2015-11-22 12:18:02 +0800

    Initial Commit

---
+++ CTReader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CTReader
+//
+//  Created by wangchaojs02 on 15/11/22.
+//  Copyright © 2015年 wangchaojs02. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CTReader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CTReader
+//
+//  Created by wangchaojs02 on 15/11/22.
+//  Copyright © 2015年 wangchaojs02. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
